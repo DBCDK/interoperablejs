5067732546e7f715f21394609534f1bf414a7dfa
Mads Bondo Dydensborg <mbd@dbc.dk>
2014-06-03 12:26:09 +0200

    Fixed to work for systems with no system.support - matches the other program.js files

--- compliance/method/program.js
+++ compliance/method/program.js
@@ -5,4 +5,4 @@ test.assert(a.foo() == a, 'calling a module member');
 test.assert(foo() != a, 'members not implicitly bound');
 a.set(10);
 test.assert(a.get() == 10, 'get and set')
-require('system').stdout.print('DONE', 'info');
+test.print('DONE', 'info');
